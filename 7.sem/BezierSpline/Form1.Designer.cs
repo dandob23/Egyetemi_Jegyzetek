027373f40f206e1831a6b1be8f4dcb0663b39160
dandob23 <103066369+dandob23@users.noreply.github.com>
2024-11-11 15:00:24 +0100

    Graf

---
+++ 7.sem/BezierSpline/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace BezierSpline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.FinalizeButton = new System.Windows.Forms.Button();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.canvas.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(628, 426);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // FinalizeButton
+            // 
+            this.FinalizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.FinalizeButton.BackColor = System.Drawing.SystemColors.Control;
+            this.FinalizeButton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FinalizeButton.Location = new System.Drawing.Point(654, 343);
+            this.FinalizeButton.Name = "FinalizeButton";
+            this.FinalizeButton.Size = new System.Drawing.Size(134, 31);
+            this.FinalizeButton.TabIndex = 1;
+            this.FinalizeButton.Text = "Finalize";
+            this.FinalizeButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.FinalizeButton.UseVisualStyleBackColor = false;
+            this.FinalizeButton.Click += new System.EventHandler(this.FinalizeButton_Click);
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ColorButton.BackColor = System.Drawing.SystemColors.Control;
+            this.ColorButton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ColorButton.Location = new System.Drawing.Point(654, 380);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(134, 31);
+            this.ColorButton.TabIndex = 2;
+            this.ColorButton.Text = "Color Spline";
+            this.ColorButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ColorButton.UseVisualStyleBackColor = false;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(654, 418);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(134, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.lineThickness_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(654, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(134, 325);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.ColorButton);
+            this.Controls.Add(this.FinalizeButton);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Colorful Bézier Spline";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button FinalizeButton;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 7.sem/BezierSpline/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BezierSpline
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = 10;
+            numericUpDown1.Value = (decimal)lineThickness;
+            numericUpDown1.ValueChanged += lineThickness_ValueChanged;
+        }
+
+        private float lineThickness = 1;
+        private List<Point> controlPoints = new List<Point>();
+        private int selectedPointIndex = -1;
+        private bool isFinalized = false;
+        private bool isColorGradient = false;
+        private List<Tuple<Color, Color>> colorPairs = new List<Tuple<Color, Color>>()
+        {
+            new Tuple<Color, Color>(Color.Red, Color.Blue),
+            new Tuple<Color, Color>(Color.Green, Color.Yellow),
+            new Tuple<Color, Color>(Color.Purple, Color.Orange),
+            new Tuple<Color, Color>(Color.Black, Color.White),
+            new Tuple<Color, Color>(Color.Gray, Color.Pink),
+            new Tuple<Color, Color>(Color.Cyan, Color.Magenta),
+            new Tuple<Color, Color>(Color.DarkBlue, Color.DarkGreen),
+            new Tuple<Color, Color>(Color.DarkRed, Color.DarkOrange),
+            new Tuple<Color, Color>(Color.DarkViolet, Color.LightYellow),
+            new Tuple<Color, Color>(Color.DarkGray, Color.DarkCyan),
+            new Tuple<Color, Color>(Color.DarkMagenta, Color.DarkTurquoise),
+            new Tuple<Color, Color>(Color.DarkSlateBlue, Color.DarkSlateGray),
+            new Tuple<Color, Color>(Color.DarkOliveGreen, Color.DarkGoldenrod),
+            new Tuple<Color, Color>(Color.DarkKhaki, Color.DarkSalmon)
+        };
+        private int hoveredPointIndex = -1;
+
+
+        private int currentColorIndex = 0;
+
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            if(controlPoints.Count>1)
+            {
+                DrawControlLines(e.Graphics);
+            }
+
+            if(isFinalized && controlPoints.Count>2)
+            {
+                DrawBezierSpline(e.Graphics);
+            }
+
+            foreach (var point in controlPoints)
+            {
+                Brush brush = Brushes.Black;
+
+                if (controlPoints.IndexOf(point) == hoveredPointIndex)
+                {
+                    brush = Brushes.Gray;
+                }
+
+                e.Graphics.FillEllipse(brush, point.X - 5, point.Y - 5, 10, 10);
+            }
+        }
+
+        private void DrawBezierSpline(Graphics g)
+        {
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                /*
+                Itt van egy hiba, mivel így 4 pontonként fog kirajzolni egy bézier görbét, 
+                amit összeköt a korábbi görbe végpontjával.
+                
+                Így nem összefüggő Bézier Spline-t kapunk, hanem több különálló Bézier görbét. 
+                 
+                
+                Megoldás: - Kivenni azt, hogy 4 pontonként rajzoljon egy Bézier görbét
+                          - Az egész controlpoints listát rajzoljuk/frissítjuk minden pontnál
+
+                 */
+                for (int i = 0; i < controlPoints.Count -3; i+=3)
+                {
+                    path.AddBezier(controlPoints[i], controlPoints[i + 1], controlPoints[i + 2], controlPoints[i + 3]);
+                }
+
+                if (isColorGradient)
+                {
+                    var colors = colorPairs[currentColorIndex];
+
+                    using (LinearGradientBrush brush = new LinearGradientBrush(
+                        new Rectangle(0,0,canvas.Width,canvas.Height),
+                        colors.Item1,colors.Item2, LinearGradientMode.Horizontal))
+                    {
+                        g.DrawPath(new Pen(brush, lineThickness), path);
+                    }
+                }
+                else
+                {
+                    g.DrawPath(new Pen(Color.Black,lineThickness),path);
+                }
+            }
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int pointIndex = FindPointAtPosition(e.Location);
+
+                //Ha a pontot megtaláljuk, akkor töröljük
+                if (pointIndex != -1)
+                {
+                    controlPoints.RemoveAt(pointIndex);
+                    canvas.Invalidate();
+                }
+                else
+                {
+                //Ha nem találjuk meg, akkor hozzáadjuk
+                controlPoints.Add(e.Location);
+                canvas.Invalidate();
+                }
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                //Ha bal kattintás történik, akkor kiválasztjuk a pontot
+                selectedPointIndex = FindPointAtPosition(e.Location);
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Ha bal kattintás történt és van kiválasztott pont, akkor mozgatjuk
+            if (selectedPointIndex != -1 && e.Button == MouseButtons.Left)
+            {
+                controlPoints[selectedPointIndex] = e.Location;
+                canvas.Invalidate();
+            }
+            else
+            {
+                //Ha nincs kiválasztott pont, akkor megnézzük, hogy van-e olyan pont, amire rámutatunk
+                hoveredPointIndex = FindPointAtPosition(e.Location);
+                canvas.Invalidate();
+            }
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Ha bal kattintás volt, akkor a kiválasztott pontot nullázuk
+            if(e.Button == MouseButtons.Left)
+            {
+                selectedPointIndex = -1;
+            }
+        }
+
+        private int FindPointAtPosition(Point location)
+        {
+            //Megkeressük azt a pontot, amelyik a megadott helyen van
+            for (int i = 0; i < controlPoints.Count; i++)
+            {
+                if (Math.Abs(controlPoints[i].X - location.X) < 10 && Math.Abs(controlPoints[i].Y - location.Y) < 10)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void FinalizeButton_Click(object sender, EventArgs e)
+        {
+            //Ha legalább 4 pont van, akkor a Bézier Spline kirajzolásra kerül
+            if(controlPoints.Count>=4)
+            {
+                isFinalized = true;
+                canvas.Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Legalább 4 pont kell a Bézier Spline kirajzolásához");
+            }
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            isColorGradient = true;
+
+            currentColorIndex = (currentColorIndex + 1) % colorPairs.Count;
+
+            canvas.Invalidate();
+        }
+
+        private void lineThickness_ValueChanged(object sender, EventArgs e)
+        {
+            lineThickness= (float)numericUpDown1.Value;
+            canvas.Invalidate();
+        }
+
+        private void DrawControlLines(Graphics g)
+        {
+            using (Pen grayPen = new Pen(Color.Gray, 1))
+            {
+                for (int i = 0; i < controlPoints.Count - 1; i++)
+                {
+                    g.DrawLine(grayPen, controlPoints[i], controlPoints[i + 1]);
+                }
+            }
+        }
+    }
+}
